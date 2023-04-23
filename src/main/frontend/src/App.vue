3496fb3a986485a083e1460d5fa65ad755b0dd5e
Elzbieta <elzbieta_warzecha@wp.pl>
2023-04-23 11:36:08 +0200

    Customer registration completed

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -8,7 +8,15 @@
     </div>
 
     <div v-else>
-      <LoginForm @login="(user) => logMeIn(user)"></LoginForm>
+      <button :class="signingUp ? 'button-outline' : ''" @click="signingUp = false">Logowanie</button> <!--  dwukropek oznacza że zaczynam pisać w JS -->
+      <button :class="!signingUp ? 'button-outline' : ''" @click="signingUp = true">Rejestracja</button>
+
+      <div v-if="message" :class="successfulRegistration ? 'alert' : 'errorAlert'">
+        {{ message }}
+      </div>
+
+      <LoginForm v-if="!signingUp" @login="(user) => logMeIn(user)"></LoginForm>
+      <LoginForm v-else @login="(user) => register(user)" button-label="Załóż konto"></LoginForm>
     </div>
   </div>
 </template>
@@ -18,12 +26,16 @@ import "milligram";
 import LoginForm from "./LoginForm";
 import UserPanel from "./UserPanel";
 import MeetingsPage from "./meetings/MeetingsPage";
+import axios from "axios";
 
 export default {
   components: {LoginForm, MeetingsPage, UserPanel},
   data() {
     return {
       authenticatedUsername: '',
+      signingUp: false, 
+      message: '',
+      successfulRegistration: false,
     }
   },
   methods: {
@@ -32,6 +44,21 @@ export default {
     },
     logMeOut() {
       this.authenticatedUsername = '';
+    },
+    register(user) {
+      axios.post('/api/participants', user)
+      .then(response => {
+          // udało się
+          //console.log("Konto założone!")
+          this.message = 'Udało się założyć konto';
+          this.successfulRegistration = true;
+      })
+      .catch(response => {
+          // nie udało sie    
+          //console.log("Błąd. Nie udało sie utworzyć konta.") 
+          this.message = 'Nie udało sie założyć konta';
+          this.successfulRegistration = false;
+      });
     }
   }
 }
@@ -41,5 +68,19 @@ export default {
 #app {
   max-width: 1000px;
   margin: 0 auto;
+}
+.alert{
+  padding: 5px;
+  margin: 30px;
+  border: 2px solid green;
+  background: lightgreen;
+  font-size: 2em
+}
+.errorAlert{
+  padding: 5px;
+  margin: 30px;
+  border: 2px solid red;
+  background: lightcoral;
+  font-size: 2em
 }
 </style>
